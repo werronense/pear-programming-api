f71bd10ec6d892a0bcf17ce44774e70578622735
your_name <alex.martishyn@gmail.com>
2024-05-16 13:56:02 -0700

    fix api and json file

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const fs = require('fs');
-const {v4: uuidv4} = require('uuid');  // npm install uuid
-const app = express();
+const { v4: uuidv4 } = require('uuid');  // npm install uuid
 const cors = require('cors');
 
+const app = express();
 const PORT = 5050;
-
 
 app.use(cors());
 app.use(express.json());
@@ -16,53 +15,49 @@ function readData() {
   return JSON.parse(data);
 }
 
-//get all pears
+// Get all pears
 app.get('/pears', (req, res) => {
   const data = readData();
   res.json(data);
 });
 
-// get two randon pears but not with the same name
+// Get two random pears but not with the same name
 app.get('/pears/random', (req, res) => {
-    const data = readData();
-    const randomIndex1 = Math.floor(Math.random() * data.length);
-    let randomIndex2 = Math.floor(Math.random() * data.length);
-    while (data[randomIndex2].name === data[randomIndex1].name) {
-      randomIndex2 = Math.floor(Math.random() * data.length);
-    }
-    res.json([data[randomIndex1], data[randomIndex2]]);
-  });
-  
-// get  pear by name filter
+  const data = readData();
+  const randomIndex1 = Math.floor(Math.random() * data.length);
+  let randomIndex2 = Math.floor(Math.random() * data.length);
+  while (data[randomIndex2].name === data[randomIndex1].name) {
+    randomIndex2 = Math.floor(Math.random() * data.length);
+  }
+  res.json([data[randomIndex1], data[randomIndex2]]);
+});
+
+// Get pear by name filter
 app.get('/pears/:name', (req, res) => {
-    const name = req.params.name + ' Pear';
-    const data = readData();
-    const pear = data.filter((pear) => pear.name === name);
-    if (pear) {
-      res.json(pear);
-    } else {
-      res.status(404).json({ message: 'Pear not found' });
-    }
-  
-  });
+  const name = req.params.name + ' Pear';
+  const data = readData();
+  const pear = data.filter((pear) => pear.name === name);
+  if (pear.length > 0) {
+    res.json(pear);
+  } else {
+    res.status(404).json({ message: 'Pear not found' });
+  }
+});
 
-// add a new pear
-// create a map of names and images
+// Add a new pear
 app.post('/pears', (req, res) => {
-    const data = readData();
-    const newPear = {
-      id: uuidv4(),
-      name: req.body.name,
-      paragraph: req.body.paragraph,
-      imageURL: `"http://localhost:5050/images/{req.body.name.toLowerCase()}-pear.jpg"`,
-    };
-    data.push(newPear);
-    fs.writeFileSync('./data/pears.json', JSON.stringify(data));
-    res.json(newPear);
-  });
-
+  const data = readData();
+  const newPear = {
+    id: uuidv4(),
+    name: req.body.name,
+    paragraph: req.body.paragraph,
+    imageUrl: `http://localhost:5050/images/${req.body.name.toLowerCase()}-pear.jpg`,
+  };
+  data.push(newPear);
+  fs.writeFileSync('./data/pears.json', JSON.stringify(data));
+  res.json(newPear);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-
+  console.log(`Server is running on port ${PORT}`);
 });
